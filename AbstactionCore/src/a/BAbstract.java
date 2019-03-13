2cb56f158fc19e7d1cef98ba91c4d7d7bd9c4171
SreedeviSa <48107804+SreedeviSa@users.noreply.github.com>
2019-03-12 22:13:29 -0400

    Modified BAbstract class

--- AbstactionCore/src/a/BAbstract.java
+++ AbstactionCore/src/a/BAbstract.java
@@ -2,6 +2,6 @@ package a;
 
 public abstract class BAbstract implements AInterface{
 	public void b() 
-	{System.out.println("I am B");}
+	{System.out.println("I am B implemented A interface");}
 
 }
